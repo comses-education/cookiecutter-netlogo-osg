df1762cfb98ffad3de002d560445fc4dd86084b3
Allen Lee <alee@users.noreply.github.com>
2023-09-19 17:50:35 -0700

    fix: add quotes to prevent whitespace creeping

--- {{ cookiecutter.model_slug }}/config.mk
+++ {{ cookiecutter.model_slug }}/config.mk
@@ -9,8 +9,10 @@ OSG_USERNAME={{ cookiecutter.osg_username }}
 # We recommend that you use all lowercase alphanumeric 
 # with - or _ characters to separate words, e.g., 
 # chime-abm, spatial-rust-model, black-hole-chaos-simulator
-MODEL_NAME={{ cookiecutter.model_slug }}  # replace with the name of your computational model. No spaces
-MODEL_FILE={{ cookiecutter.model_file }}  # replace with your main NetLogo entrypoint file.
+# replace with the name of your computational model. No spaces
+MODEL_NAME="{{ cookiecutter.model_slug }}"
+# replace with your main NetLogo entrypoint file.
+MODEL_FILE={{ cookiecutter.model_file }}
 # single behavior space experiment to run
 # for multiple behavior space experiments, use a queue directive like
 # `queue experiment from experiments.txt`
@@ -19,9 +21,9 @@ MODEL_FILE={{ cookiecutter.model_file }}  # replace with your main NetLogo entry
 BEHAVIOR_SPACE_EXPERIMENT={{ cookiecutter.behavior_space_experiment }}
 ENTRYPOINT_SCRIPT={{ cookiecutter.entrypoint_script }}
 # the submit file to be executed on OSG via `condor_submit ${OSG_SUBMIT_FILE}`
-OSG_SUBMIT_FILENAME=${MODEL_NAME}.sub
+OSG_SUBMIT_FILENAME="{{ cookiecutter.model_slug }}.sub"
 # Condor submit script's queue directive
 #
 # for multiple behavior space experiments, use a queue directive like
 # OSG_QUEUE_DIRECTIVE="experiment from experiments.txt"
-OSG_QUEUE_DIRECTIVE={{ cookiecutter.osg_queue_directive }}
+OSG_QUEUE_DIRECTIVE="{{ cookiecutter.osg_queue_directive }}"
